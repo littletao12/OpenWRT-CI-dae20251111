faadc6aa6030555c7535c9af00919bc04eb071ec
littletao12 <166675326+littletao12@users.noreply.github.com>
2025-11-17 17:35:00 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -44,12 +44,12 @@ if [ -f "$NSS_PBUF" ]; then
 	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
 fi
 
-#修复TailScale配置文件冲突
-TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
-if [ -f "$TS_FILE" ]; then
-	echo " "
+# #修复TailScale配置文件冲突
+# TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
+# if [ -f "$TS_FILE" ]; then
+# 	echo " "
 
-	sed -i '/\/files/d' $TS_FILE
+# 	sed -i '/\/files/d' $TS_FILE
 
-	cd $PKG_PATH && echo "tailscale has been fixed!"
-fi
+# 	cd $PKG_PATH && echo "tailscale has been fixed!"
+# fi
